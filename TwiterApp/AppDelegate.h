4d585f2a3c7775c58a6eb9659fbe14fae9354190
Fábio Moreira <fabio@mac-mini-gama.local>
2017-06-28 10:41:14 -0300

    Initial Commit

---
+++ TwiterApp/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  TwiterApp
+//
+//  Created by Fábio Moreira on 6/28/17.
+//  Copyright © 2017 Fábio Moreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ TwiterApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TwiterApp
+//
+//  Created by Fábio Moreira on 6/28/17.
+//  Copyright © 2017 Fábio Moreira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
